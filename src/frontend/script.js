374cfdb14080ee71525d3508968863645f538c2e
sumit-mashelkar <mashelkarsumit@gmail.com>
2026-07-23 21:06:13 +0530

    Implement modal for bus route search and enhance styling

--- src/frontend/script.js
+++ src/frontend/script.js
@@ -1,10 +1,9 @@
-//const button = document.getElementById("menu");
-const title = document.getElementById("title");
-//const menuButton = document.getElementById("menu");
-//const sidePanel = document.getElementById("side-panel");
-const seachButton = document.getElementById("seach-button");
-
-//inner HTML to replace search form with bus results
+const title = document.getElementById("title");//1
+//const menuButton = document.getElementById("menu");//2
+//const sidePanel = document.getElementById("side-panel");//3
+const seachButton = document.getElementById("seach-button");//4
+const handle = document.getElementById("handle");//5
+//const button = document.getElementById("menu");//6
 
 
 //menu button
@@ -21,10 +20,37 @@ const seachButton = document.getElementById("seach-button");
 
 
 //search button operation
-seachButton.addEventListener("click",function (){
-  console.log("loading busses...");
-  document.getElementById("main").innerHTML=
-'<link rel="stylesheet" href="Bustyle.css"/><div id="title">Where is my Bus</div>';
-  //window.location.href="Bus.html";
-  console.log("loaded bus page");
+// seachButton.addEventListener("click",function (){
+//   console.log("loading busses...");
+//   document.getElementById("main").innerHTML=
+// `
+  
+//   <div id="result">
+//   <h1>no bus found</h1>
+//   </div>
+
+//   //window.location.href="Bus.html";
+//   console.log("loaded bus page");
+// `;  
+// });
+
+//modal to search the bus
+const modal = document.getElementById("modal");
+
+document.getElementById("openModal").addEventListener("click",function(){
+
+    modal.classList.add("show");
+
 });
+
+document.getElementById("searchBtn").addEventListener("click",function(){
+
+    modal.classList.remove("show");
+
+});
+
+
+//handle
+handle.getElementById("click", function(){
+    console.log("closing the modal");
+})
