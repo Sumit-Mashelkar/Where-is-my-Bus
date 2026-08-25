72ed5f79993d787853994a97ed02491b15f8886a
sumit-mashelkar <mashelkarsumit@gmail.com>
2026-08-25 23:31:23 +0530

    feat: enhance AddRoutesPage and ReportsPage with Hero and Footer components

--- transitpulse/src/pages/AddRoutesPage.jsx
+++ transitpulse/src/pages/AddRoutesPage.jsx
@@ -1,9 +1,14 @@
 // contains the page for adding new bus routes
-
+import Hero from "../components/header";
+import Footer from "../components/footer";
 
 const AddRoutesPage = () => {
     return(
+        <>
+        <Hero />
         <div>add a new route</div>
+        <Footer />
+        </>
     )
 
 }
--- transitpulse/src/pages/ReportsPage.jsx
+++ transitpulse/src/pages/ReportsPage.jsx
@@ -1,8 +1,15 @@
 // contains all bus reports
+import Hero from "../components/header";
+import Footer from "../components/footer";
 
 const ReportBusPage = () => {
     return (
+        <>
+        <Hero />
         <div>report a bus</div>
+        <Footer />
+        </>
+        
     )
 }
 
