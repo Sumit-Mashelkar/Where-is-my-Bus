9d8f6572cb3b556059a11af46defb4cfcd0652c2
sumit-mashelkar <mashelkarsumit@gmail.com>
2026-08-25 23:21:11 +0530

    feat: add AddRoutesPage and ReportsPage components for route management

---
+++ transitpulse/src/pages/AddRoutesPage.jsx
@@ -0,0 +1,10 @@
+// contains the page for adding new bus routes
+
+
+const AddRoutesPage = () => {
+    return(
+        <div>add a new route</div>
+    )
+
+}
+export default AddRoutesPage;
---
+++ transitpulse/src/pages/ReportsPage.jsx
@@ -0,0 +1,9 @@
+// contains all bus reports
+
+const ReportBusPage = () => {
+    return (
+        <div>report a bus</div>
+    )
+}
+
+export default ReportBusPage;
